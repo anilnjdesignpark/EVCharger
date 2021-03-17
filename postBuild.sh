86207fa8eec8497fce35a50fbfaafceef728bf0e
anilnjdesignpark <Anil.njdesignpark@gmail.com>
2021-03-17 16:43:44 +0530

    Adding postbuild.sh

--- postBuild.sh
+++ postBuild.sh
@@ -1,1 +1,8 @@
-mv "./build/static/css/main*css" "main.css"
+mv ./build/static/css/main*css ./build/static/css/main.css
+
+mv ./build/static/js/2*js ./build/static/js/app.js
+
+mv ./build/static/js/main*js ./build/static/js/main.js
+
+mv ./build/static/js/runtime*js ./build/static/js/runtime.js
+
